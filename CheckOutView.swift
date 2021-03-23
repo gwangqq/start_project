ab491e8d496781a71fd75888108c2fa5396de600
gwanggyu park <pgg961@gmail.com>
2021-03-24 00:19:18 +0900

    working on order view

--- CheckOutView.swift
+++ CheckOutView.swift
@@ -12,17 +12,41 @@ struct CheckOutView: View {
     // paymennt types
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     @State private var paymentType = "Cash"
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 15
     
+    let tipAmounts = [10, 15, 20, 25, 0]
     
     var body: some View {
-        VStack{
+        Form{
             Section{
                 Picker("How do you want to pay?", selection: $paymentType){
                     ForEach(paymentTypes, id: \.self){
                         Text($0)
                     }
                 }
+                Toggle("Add iDine loyalty card", isOn:$addLoyaltyDetails.animation())
+                if addLoyaltyDetails{
+                    TextField("Enter your iDine ID", text:$loyaltyNumber)
+                }
+                
             }
+            Section(header: Text("Add a tip?")){
+                Picker("Percentage:", selection: $tipAmount){
+                    ForEach(tipAmounts, id:\.self){
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("TOTLA: $100")){
+                Button("Confirm Order"){
+                    // place the order
+                }
+            }
+            
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
